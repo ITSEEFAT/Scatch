e2c9195a2e7d01f7e17fa564650193ed65393684
Deeba45 <160336506+Deeba45@users.noreply.github.com>
2025-08-02 13:54:46 +0530

    Secure password hashing with bcryptjs

--- models/user-model.js
+++ models/user-model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
-let userSchma = mongoose.Schema({
+const bcrypt = require("bcryptjs");
+const isMatch = await bcrypt.compare(inputPassword, user.password);
+let userSchema = mongoose.Schema({
   fullname: {
     type: String,
     minLength: 3,
@@ -15,41 +16,36 @@ let userSchma = mongoose.Schema({
     },
   ],
   order: {
-    type:Array,
-    default:[],
+    type: Array,
+    default: [],
   },
   contact: {
-    type:Number,
-    default: +91, 
-    minLength:10,
+    type: Number,
+    default: +91,
+    minLength: 10,
   },
   picture: {
-    type:Buffer,
-    default:Buffer.alloc(0),
+    type: Buffer,
+    default: Buffer.alloc(0),
   },
   address: {
-    village: {
-      type: String,
-      default: ""
-    },
-    post: {
-      type: String,
-      default: ""
-    },
-    district: {
-      type: String,
-      default: ""
-    },
-    state: {
-      type: String,
-      default: ""
-    }
+    village: { type: String, default: "" },
+    post: { type: String, default: "" },
+    district: { type: String, default: "" },
+    state: { type: String, default: "" },
   },
   date: {
     type: Date,
     default: Date.now,
   },
-
 });
 
-module.exports = mongoose.model("user", userSchma);
+// 🔐 Hash password before saving (only if modified)
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+module.exports = mongoose.model("user", userSchema);
